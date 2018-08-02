8c9605653d8a87ce27e0d809ba5306797ebb2c67
msfarookhy <msfarookhy@gmail.com>
2018-08-02 12:08:57 +0530

    tax

---
+++ tax.js
@@ -0,0 +1,17 @@
+function sum(tax){
+  var tax =1.552;
+  console.log(amount=tax*amount);
+
+}
+var amount= 19999 ;
+sum(amount.toFixed());
+*/
+
+/*
+function printAmount(){
+  console.log(amount. toFixed(2))
+  
+}
+var amount=10.502174
+printAmount()
+
