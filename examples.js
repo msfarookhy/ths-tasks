e9720db53b2ef882cf380703a8047eb2fa371113
msfarookhy <msfarookhy@gmail.com>
2018-08-06 17:38:09 +0530

    new projects

---
+++ examples.js
@@ -0,0 +1,23 @@
+
+let hello = "Hello";
+
+
+function sayHi(){
+
+    hello = "hi";
+    console.log(hello);
+
+}
+sayHi();
+console.log(hello);
+//
+// Anaylysisi of alogorithm
+// time complexctiy
+// json
+// es6
+// block scope
+// var
+//     let
+// constant in scopes
+// array methodes
+// controlli with block scoping
